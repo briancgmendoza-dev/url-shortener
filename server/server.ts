39d08b8235b8c6e30653d92628c0b37974bdd5a6
Brian Christopher Mendoza <briancgmendoza@gmail.com>
2025-05-04 07:04:20 +0800

    [Feature] Updated server file

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,32 @@
-console.log("A")
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDb } from './database/db';
+import urlRoutes from './routes/url-routes'
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', urlRoutes)
+
+app.use('/', (req, res) => {
+  res.send('URL Shortener API is up and running.')
+})
+
+const startServer = async (): Promise<void> => {
+  try {
+    await connectDb()
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`)
+    })
+  } catch (error) {
+    console.error('Error starting server: ', error)
+  }
+}
+
+startServer();
