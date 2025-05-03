860cd5d685da372205ddd4923ee7852e8cff6433
Brian Christopher Mendoza <briancgmendoza@gmail.com>
2025-05-04 07:06:28 +0800

    [Feature] Adding cors allow all origin

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDb } from './database/db';
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { connectDb } from './database/db'
 import urlRoutes from './routes/url-routes'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(cors())
+
+app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -29,4 +32,4 @@ const startServer = async (): Promise<void> => {
   }
 }
 
-startServer();
+startServer()
